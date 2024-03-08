e02975ef42d1865d440b56bee65a40ba48481e7d
zkociuba <102521842+zkociuba@users.noreply.github.com>
2024-03-08 11:21:23 +0100

    Update 08.03.sql

--- 08.03.sql
+++ 08.03.sql
@@ -1,1 +1,39 @@
 SELECT * FROM world.country
+
+SELECT
+GNP AS PKB,
+Name AS Kontynent,
+Population AS Populacja,
+SurfaceArea AS Powierzchnia,
+LifeExpectancy AS DlugoscZycia
+FROM
+country;
+
+-sortowanie według kraju z najwiekszym PKB
+SELECT
+GNP AS PKB,
+Name AS Kontynent,
+Population AS Populacja,
+SurfaceArea AS Powierzchnia,
+LifeExpectancy AS DlugoscZycia
+FROM
+country
+ORDER BY PKB DESC;
+
+-tworzenie widoku
+CREATE VIEW PozycjaPKB AS
+SELECT
+GNP AS PKB,
+Name AS Kontynent,
+Population AS Populacja,
+SurfaceArea AS Powierzchnia,
+LifeExpectancy AS DlugoscZycia
+FROM
+country
+ORDER BY PKB DESC;
+
+
+SELECT * FROM world.pozycjapkb;
+
+SELECT PKB, Kraj FROM world.pozycjapkb;
+
